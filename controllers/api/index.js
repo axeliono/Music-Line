da4ae7fdb9795c8547f1cf26ecf14d3dd54bf683
axeliono <69375446+axeliono@users.noreply.github.com>
2021-01-19 19:54:15 -0600

    basics for routes done

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,15 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const userRoutes = require('./user-routes.js');
-const classificationRoutes = require('./classification-routes');
-const saleRoutes = require('./sale-routes');
-const instrumentRoutes = require('./instrument-routes');
-const shoppingCartRoutes = require('./shopping_cart_selection-routes');
+const userRoutes = require("./user-routes.js");
+const classificationRoutes = require("./classification-routes");
+const saleRoutes = require("./sale-routes");
+const instrumentRoutes = require("./instrument-routes");
+const shoppingCartRoutes = require("./shopping-routes");
 
-router.use('/users', userRoutes);
+router.use("/users", userRoutes);
+router.use("/classification", classificationRoutes);
+router.use("/sale", saleRoutes);
+router.use("/instrument", instrumentRoutes);
+router.use("/shopping", shoppingCartRoutes);
 
 module.exports = router;
---
+++ controllers/api/instrument-routes.js
@@ -0,0 +1,77 @@
+const { Instrument } = require("../../models");
+const router = require("express").Router();
+
+//GET all Instruments
+router.get("/", (req, res) => {
+  Instrument.findAll({
+    //input attributes later if needed
+  })
+    .then((dbInstrumentData) => {
+      res.json(dbInstrumentData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get("/:id", (req, res) => {
+  Instrument.findOne({
+    //input attributes later if needed
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbInstrumentData) => {
+      if (!dbInstrumentData) {
+        res.status(404).json({ message: "no Instrument found with this id" });
+        return;
+      }
+      res.json(dbInstrumentData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+//Create new Instrument
+router.post("/", (req, res) => {
+  Instrument.create({
+    name: req.body.name,
+    classification_id: req.body.classification_id,
+    origin: req.body.origin,
+    price: req.body.price,
+    manufacturer: req.body.manufacturer,
+  })
+    .then((dbInstrumentData) => {
+      res.json(dbInstrumentData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+
+//Delete Instrument
+router.delete("/:id", (req, res) => {
+  Instrument.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbInstrumentData) => {
+      if (!dbInstrumentData) {
+        res.status(404).json({ message: "No Instrument found with this id" });
+        return;
+      }
+
+      res.json(dbInstrumentData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
