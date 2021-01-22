1607ba62dcab7eecaece576cbad6e9c6881de689
frecklescherokee <71302040+frecklescherokee@users.noreply.github.com>
2021-01-21 20:41:30 -0600

    added seed data for classifications and instruments

---
+++ seeds/index.js
@@ -0,0 +1,26 @@
+const seedClassifications = require('./classification-seeds');
+const seedInstruments = require('./instrument-seeds');
+const seedSales = require('./sale-seeds');
+const seedShoppingCart = require('./shopping-cart-seeds');
+
+const sequelize = require('../config/connection');
+
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+  console.log('\n----- DATABASE SYNCED -----\n');
+  await seedClassifications();
+  console.log('\n----- CLASSIFICATIONS SEEDED -----\n');
+
+  await seedInstruments();
+  console.log('\n----- INSTRUMENTS SEEDED -----\n');
+
+  await seedSales();
+  console.log('\n----- SALES SEEDED -----\n');
+
+  await seedShoppingCart();
+  console.log('\n----- SHOPPING CART SELECTIONS SEEDED -----\n');
+
+  process.exit(0);
+};
+
+seedAll();
